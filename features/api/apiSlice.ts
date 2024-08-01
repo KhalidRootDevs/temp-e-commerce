29a260a02ddf21fee487cb09580c1a288fb9d32b
khalidrootdevs <khalidrootdevs@gmail.com>
2024-08-01 18:53:08 +0600

    bearer token added

--- features/api/apiSlice.ts
+++ features/api/apiSlice.ts
@@ -1,9 +1,16 @@
+import { auth } from '@/auth';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
   // @ts-ignore
   baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
   prepareHeaders: async (headers) => {
+    const session = await auth();
+
+    if (session?.user) {
+      headers.set('Authorization', `Bearer ${session.user.accessToken}`);
+    }
+
     headers.set(
       'x-api-key',
       process.env.NEXT_PUBLIC_API_KEY || process.env.API_KEY || ''
